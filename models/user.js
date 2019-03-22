acbe629b60b1f2ffe7890381b9897e4e549b9c05
Andrew Wanex <awanex2705@gmail.com>
2019-03-22 12:55:23 -0700

    feat: add user schema

---
+++ models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const recipeMasterSchema = require('../models/recipe-master');
+
+const userSchema = new Schema({
+  firstname: {
+    type: String,
+    default: ''
+  },
+  lastname: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  masterList: [recipeMasterSchema]
+}, {
+  timestamps: true
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
