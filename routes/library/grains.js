2f07ff0927e23f2bac5d555cd4c145f752246a4c
Andrew Wanex <awanex2705@gmail.com>
2019-03-22 12:56:51 -0700

    feat: add routes for grains library

---
+++ routes/library/grains.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const createError = require('http-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const authenticate = require('../../authenticate');
+const Grains = require('../../models/grains');
+
+const grainsRouter = express.Router();
+
+grainsRouter.use(bodyParser.json());
+
+grainsRouter.route('/')
+  .get((req, res, next) => {
+    Grains.find({})
+      .then(grains => {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        res.json(grains);
+      }, error => next(error))
+      .catch(error => next(error));
+  })
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Grains.create(req.body)
+      .then(grains => {
+        res.statusCode = 201;
+        res.setHeader('content-type', 'application/json');
+        res.json(grains);
+      }, error => next(error))
+      .catch(error => next(error));
+  });
+
+grainsRouter.route('/:grainsId')
+  .get((req, res, next) => {
+    Malts.findById(req.params.grainsId)
+      .then(grains => {
+        if (grains != null) {
+          res.statusCode = 200;
+          res.setHeader('content-type', 'application/json');
+          res.json(grains);
+        } else {
+          return next(createError(404));
+        }
+      }, error => next(error))
+      .catch(error => next(error))
+  })
+  .patch(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Grains.findByIdAndUpdate(req.params.grainsId, req.body, {new: true})
+      .then(grains => {
+        if (grains != null) {
+          res.statusCode = 200;
+          res.setHeader('content-type', 'application/json');
+          res.json(grains);
+        } else {
+          return next(createError(404));
+        }
+      }, error => next(error))
+      .catch(error => next(error));
+  })
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Grains.findByIdAndDelete(req.params.grainsId)
+      .then(dbres => {
+        if (dbres != null) {
+          res.statusCode = 200;
+          res.setHeader('content-type', 'application/json');
+          res.json(dbres);
+        } else {
+          return next(createError(404));
+        }
+      }, error => next(error))
+      .catch(error => next(error));
+  });
+
+module.exports = grainsRouter;
