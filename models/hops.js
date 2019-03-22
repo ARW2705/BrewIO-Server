314008ba79b412267203400522616047ae56b17b
Andrew Wanex <awanex2705@gmail.com>
2019-03-22 12:52:22 -0700

    feat: add schema for hops library entry

---
+++ models/hops.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const hopsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  alphaAcid: {
+    type: Number,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  usedFor: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Style'
+  }],
+  alternatives: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Hops'
+  }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Hops', hopsSchema);
