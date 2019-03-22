10df7c20e3ce48a2943732a7294283199e3f2920
Andrew Wanex <awanex2705@gmail.com>
2019-03-22 12:55:57 -0700

    feat: add schema for recipe yeast

---
+++ models/yeast-batch.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const yeastBatchSchema = new Schema({
+  yeastType: {
+    type: Schema.Types.ObjectId,
+    ref: 'Yeast'
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  requiresStarter: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+module.exports = yeastBatchSchema;
