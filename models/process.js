15c5280c063c2c4db4fc8ea748b83185e18c6f00
Andrew Wanex <awanex2705@gmail.com>
2019-04-11 18:17:56 -0700

    refactor: combine properties of different step types - add type property

--- models/process.js
+++ models/process.js
@@ -8,9 +8,46 @@ const timerStepSchema = require('./timer-step');
 const calendarStepSchema = require('./calendar-step');
 
 const processSchema = new Schema({
-  manualSteps: [manualStepSchema],
-  timerSteps: [timerStepSchema],
-  calendarSteps: [calendarStepSchema]
+  type: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  order: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  startDatetime: {
+    type: Date
+  },
+  endDatetime: {
+    type: Date
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  splitInterval: {
+    type: Number,
+    default: 1
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  concurrent: {
+    type: Boolean,
+    default: false
+  },
+  expectedDuration: {
+    type: Number
+  }
 });
 
 module.exports = processSchema;
