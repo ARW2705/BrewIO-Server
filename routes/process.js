f4872465c5df2dc929319ef11381314c1492a599
Andrew Wanex <awanex2705@gmail.com>
2019-04-23 16:55:58 -0700

    refactor: fix id names and save process changes to db

--- routes/process.js
+++ routes/process.js
@@ -14,113 +14,104 @@ const processRouter = express.Router();
 
 processRouter.use(bodyParser.json());
 
-processRouter.route('/master/:masterId/recipe/:recipeId')
+processRouter.route('/master/:masterRecipeId/recipe/:recipeId')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findOne({_id: req.user.id, masterList: req.params.masterRecipeId})
       .then(user => {
-        if (user != null) {
-          return RecipeMaster.findById(req.params.masterRecipeId)
-            .then(master => {
-              if (master) {
-                const recipe = master.recipes.find(item => item.equals(req.params.recipeId));
-                return Recipe.findById(recipe)
-                  .then(recipe => {
+        return RecipeMaster.findById(req.params.masterRecipeId)
+          .then(master => {
+            if (master) {
+              const recipe = master.recipes.find(item => item.equals(req.params.recipeId));
+              return Recipe.findById(recipe)
+                .then(recipe => {
+                  if (recipe) {
                     res.statusCode = 200;
                     res.setHeader('content-type', 'application/json');
                     res.json(recipe.processSchedule);
-                  });
-              } else {
-                const err = new Error(`Batch with id: "${req.params.recipeId}" not found`);
-                err.status = 404;
-                return next(err);
-              }
-            });
-        } else {
-          const err = new Error('Could not find user');
-          err.status = 404;
-          return next(err);
-        }
+                  } else {
+                    const err = new Error(`Batch with id: "${req.params.recipeId}" not found`);
+                    err.status = 404;
+                    return next(err);
+                  }
+                });
+            } else {
+              const err = new Error(`Recipe with id: "${req.params.masterRecipeId}" not found`);
+              err.status = 404;
+              return next(err);
+            }
+          });
       })
       .catch(err => next(err));
   });
 
-processRouter.route('/master/:masterId/recipe/:recipeId/step/:stepId')
+processRouter.route('/master/:masterRecipeId/recipe/:recipeId/step/:stepId')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findOne({_id: req.user.id, masterList: req.params.masterRecipeId})
       .then(user => {
-        if (user) {
-          return RecipeMaster.findById(req.params.masterRecipeId)
-            .then(master => {
-              if (master) {
-                const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
-                return Recipe.findById(_recipe)
-                  .then(recipe => {
-                    if (recipe) {
-                      const step = recipe.processSchedule.find(step => step.equals(req.params.stepId));
-                      if (step) {
-                        res.statusCode = 200;
-                        res.setHeader('content-type', 'application/json');
-                        res.json(step);
-                      } else {
-                        const err = new Error(`Step with id: "${req.params.stepId}" not found`);
-                        err.status = 404;
-                        return next(err);
-                      }
+        return RecipeMaster.findById(req.params.masterRecipeId)
+          .then(master => {
+            if (master) {
+              const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
+              return Recipe.findById(_recipe)
+                .then(recipe => {
+                  if (recipe) {
+                    const step = recipe.processSchedule.find(step => step.equals(req.params.stepId));
+                    if (step) {
+                      res.statusCode = 200;
+                      res.setHeader('content-type', 'application/json');
+                      res.json(step);
                     } else {
-                      const err = new Error(`Recipe with id: "${req.params.recipeId}" not found`);
+                      const err = new Error(`Step with id: "${req.params.stepId}" not found`);
                       err.status = 404;
                       return next(err);
                     }
-                  });
-              } else {
-                const err = new Error(`Master with id: "${req.params.masterId}" not found`);
-                err.status = 404;
-                return next(err);
-              }
-            });
+                  } else {
+                    const err = new Error(`Recipe with id: "${req.params.recipeId}" not found`);
+                    err.status = 404;
+                    return next(err);
+                  }
+                });
+            } else {
+              const err = new Error(`Master with id: "${req.params.masterRecipeId}" not found`);
+              err.status = 404;
+              return next(err);
+            }
+          });
       })
       .catch(err => next(err));
   })
   .patch(authenticate.verifyUser, (req, res, next) => {
     User.findOne({_id: req.user.id, masterList: req.params.masterRecipeId})
       .then(user => {
-        if (user != null) {
-          return RecipeMaster.findById(req.params.masterRecipeId)
-            .then(master => {
-              if (master) {
-                const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
-                return Recipe.findById(_recipe)
-                  .then(recipe => {
-                    if (recipe) {
-                      const toUpdate = recipe.processSchedule.find(step => step.equals(req.params.stepId));
-                      console.log('pre', toUpdate);
-                      for (const key in req.body) {
-                        if (key == '_id') continue;
-                        toUpdate[key] = req.body[key];
-                      }
-                      console.log('post', toUpdate);
-                      return recipe.save()
-                        .then(dbres => {
-                          res.statusCode = 200;
-                          res.setHeader('content-type', 'application/json');
-                          res.json(toUpdate);
-                        });
-                    } else {
-                      const err = new Error(`Batch with id: "${req.params.recipeId}" not found`);
-                      err.status = 404;
-                      return next(err);
-                  });
-              } else {
-                const err = new Error(`Recipe with id: "${req.params.masterId}" not found`);
-                err.status = 404;
-                return next(err);
-              }
-            });
-        } else {
-          const err = new Error('Could not find user');
-          err.status = 404;
-          return next(err);
-        }
+        return RecipeMaster.findById(req.params.masterRecipeId)
+          .then(master => {
+            if (master) {
+              const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
+              return Recipe.findById(_recipe)
+                .then(recipe => {
+                  if (recipe) {
+                    const updateIndex = recipe.processSchedule.findIndex(step => step.equals(req.params.stepId));
+                    for (const key in req.body) {
+                      recipe.processSchedule[updateIndex][key] = req.body[key];
+                    }
+                    return recipe.save()
+                      .then(dbres => {
+                        res.statusCode = 200;
+                        res.setHeader('content-type', 'application/json');
+                        res.json(dbres.processSchedule[updateIndex]);
+                      });
+                  } else {
+                    const err = new Error(`Batch with id: "${req.params.recipeId}" not found`);
+                    err.status = 404;
+                    return next(err);
+                  }
+                });
+            } else {
+              const err = new Error(`Recipe with id: "${req.params.masterRecipeId}" not found`);
+              err.status = 404;
+              return next(err);
+            }
+          });
       })
       .catch(err => next(err));
   });
