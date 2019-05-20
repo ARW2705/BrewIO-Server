80c2441e9f4aaf206d63e7a4f65574e4144b6af4
Andrew Wanex <awanex2705@gmail.com>
2019-05-20 16:36:48 -0700

    refactor: remove process from the recipe to an arry attached to the user to allow concurrent batches

---
+++ models/in-progress.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const processSchema = require('./process');
+const alertSchema = require('./alert');
+
+const inProgressSchema = new Schema({
+  currentStep: {
+    type: Number,
+    default: 0
+  },
+  startDateTime: {
+    type: Date
+  },
+  alerts: [alertSchema],
+  recipe: {
+    type: Schema.Types.ObjectId,
+    ref: 'recipe',
+    required: true
+  },
+  schedule: [processSchema]
+}, {
+  timestamps: true
+});
+
+module.exports = inProgressSchema;
