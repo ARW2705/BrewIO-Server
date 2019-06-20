2755b54340f7b1149eba139b2c5759cb74368424
Andrew Wanex <awanex2705@gmail.com>
2019-06-20 13:46:55 -0700

    refactor: update process routes to conform to new process model

--- routes/process.js
+++ routes/process.js
@@ -15,7 +15,7 @@ const processRouter = express.Router();
 
 processRouter.use(bodyParser.json());
 
-processRouter.route('in-progress/:batchId')
+processRouter.route('/in-progress/:batchId')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findById(req.user.id)
       .then(user => {
@@ -59,10 +59,13 @@ processRouter.route('in-progress/:batchId')
         if (toDeleteIndex != -1) {
           user.inProgressList.splice(toDeleteIndex, 1);
           return user.save()
-            .then(() => {
+            .then(dbres => {
               res.statusCode = 200;
               res.setHeader('content-type', 'application/json');
-              res.json({success: true});
+              res.json({
+                success: true,
+                updatedList: user.inProgressList
+              });
             });
         } else {
           const err = new Error(`Batch with id ${req.params.batchId} not found`);
@@ -73,7 +76,7 @@ processRouter.route('in-progress/:batchId')
       .catch(error => next(error));
   });
 
-processRouter.route('batch/:batchId/step/:stepId')
+processRouter.route('/in-progress/:batchId/step/:stepId')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findById(req.user.id)
       .then(user => {
@@ -87,12 +90,12 @@ processRouter.route('batch/:batchId/step/:stepId')
           } else {
             const err = new Error(`Step with id ${req.params.stepId} not found`);
             err.status = 404;
-            return err;
+            return next(err);
           }
         } else {
           const err = new Error(`Batch with id ${req.params.batchId} not found`);
           err.status = 404;
-          return err;
+          return next(err);
         }
       })
       .catch(error => next(error))
@@ -104,28 +107,37 @@ processRouter.route('batch/:batchId/step/:stepId')
         if (batch) {
           const stepIndex = batch.schedule.findIndex(item => item.equals(req.params.stepId));
           if (stepIndex != -1) {
-            batch[stepIndex] = req.body;
+            if (req.body.alerts) {
+              const alertsToAdd = batch.alerts.filter(alert => {
+                return alert.title != req.body.alerts[0].title
+              })
+              .concat(req.body.alerts);
+              batch.alerts = alertsToAdd;
+            }
+            if (req.body.startDatetime) {
+              batch.schedule[stepIndex]['startDatetime'] = req.body.startDatetime;
+            }
             return user.save()
               .then(dbres => {
                 res.statusCode = 200;
                 res.setHeader('content-type', 'application/json');
-                res.json(dbres);
+                res.json(batch);
               });
           } else {
             const err = new Error(`Step with id ${req.params.stepId} not found`);
             err.status = 404;
-            return err;
+            return next(err);
           }
         } else {
           const err = new Error(`Batch with id ${req.params.batchId} not found`);
           err.status = 404;
-          return err;
+          return next(err);
         }
       })
       .catch(error => next(error))
   });
 
-processRouter.route('batch/:batchId/next')
+processRouter.route('/in-progress/:batchId/next')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findById(req.user.id)
       .then(user => {
@@ -141,27 +153,30 @@ processRouter.route('batch/:batchId/next')
         } else {
           const err = new Error(`Batch with id ${req.params.batchId} not found`);
           err.status = 404;
-          return err;
+          return next(err);
         }
       })
       .catch(error => next(error))
   });
 
-processRouter.route('user/:userId/master/:masterRecipeId/recipe/:recipeId')
+processRouter.route('/user/:userId/master/:masterRecipeId/recipe/:recipeId')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findById(req.params.userId)
       .then(user => {
         if (user) {
           return RecipeMaster.findOne({
-            _id: req.params.masterRecipeId,
-            $or: [
-              { isPublic: true },
-              {
-                $and: [
-                  { isFriendsOnly: true },
-                  { friendList: req.user.id }
-                ]
-              }
+            $and: [
+              {_id: req.params.masterRecipeId},
+              {$or: [
+                { owner: req.user.id },
+                { isPublic: true },
+                {
+                  $and: [
+                    { isFriendsOnly: true },
+                    { friendList: req.user.id }
+                  ]
+                }
+              ]}
             ]
           })
           .populate({
@@ -198,48 +213,50 @@ processRouter.route('user/:userId/master/:masterRecipeId/recipe/:recipeId')
             }
           })
           .then(master => {
+            console.log('found master', master);
             if (master) {
               const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
               return Recipe.findById(_recipe)
                 .then(recipe => {
                   if (recipe) {
-                    return InProgress.create({
-                      recipe: recipe._id,
-                      schedule: Array.from(recipe.processSchedule, process => {
-                        const copy = {};
-                        for (const key in process) {
-                          if (key != '_id') {
-                            copy[key] = process[key];
-                          }
-                        }
-                        return copy;
-                      })
-                    })
-                      .then(startedBatch => {
-                        return User.findById(req.user.id)
-                          .then(requestingUser => {
-                            requestingUser.inProgressList.push(startedBatch);
-                            return requestingUser.save()
-                              .then(() => {
-                                res.statusCode = 200;
-                                res.setHeader('content-type', 'application/json');
-                                res.json(startedBatch);
-                              })
+                    User.findById(req.user.id)
+                      .then(requestingUser => {
+                        const newInProgress = {
+                          recipe: recipe._id,
+                          schedule: Array.from(recipe.processSchedule, process => {
+                            const copy = {};
+                            for (const key in process) {
+                              if (key != '_id') {
+                                copy[key] = process[key];
+                              }
+                            }
+                            return copy;
+                          })
+                        };
+                        requestingUser.inProgressList.push(newInProgress);
+                        return requestingUser.save()
+                          .then(dbres => {
+                            res.statusCode = 200;
+                            res.setHeader('content-type', 'application/json');
+                            res.json(dbres.inProgressList);
                           })
                       })
                   } else {
+                    console.log('recipe not found');
                     const err = new Error(`Recipe with id: "${req.params.recipeId}" not found`);
                     err.status = 404;
                     return next(err);
                   }
                 })
             } else {
+              console.log('master not found');
               const err = new Error(`Recipe master with id: "${req.params.masterRecipeId}" not found`);
               err.status = 404;
               return next(err);
             }
           })
         } else {
+          console.log('user not found');
           const err = new Error(`User with id: "${req.params.userId}" not found`);
           err.status = 404;
           return next(err);
@@ -247,107 +264,5 @@ processRouter.route('user/:userId/master/:masterRecipeId/recipe/:recipeId')
       })
       .catch(error => next(error))
   });
-
-// processRouter.route('/master/:masterRecipeId/recipe/:recipeId')
-//   .get(authenticate.verifyUser, (req, res, next) => {
-//     User.findOne({_id: req.user.id, masterList: req.params.masterRecipeId})
-//       .then(user => {
-//         return RecipeMaster.findById(req.params.masterRecipeId)
-//           .then(master => {
-//             if (master) {
-//               const recipe = master.recipes.find(item => item.equals(req.params.recipeId));
-//               return Recipe.findById(recipe)
-//                 .then(recipe => {
-//                   if (recipe) {
-//                     res.statusCode = 200;
-//                     res.setHeader('content-type', 'application/json');
-//                     res.json(recipe.processSchedule);
-//                   } else {
-//                     const err = new Error(`Batch with id: "${req.params.recipeId}" not found`);
-//                     err.status = 404;
-//                     return next(err);
-//                   }
-//                 });
-//             } else {
-//               const err = new Error(`Recipe with id: "${req.params.masterRecipeId}" not found`);
-//               err.status = 404;
-//               return next(err);
-//             }
-//           });
-//       })
-//       .catch(err => next(err));
-//   });
-//
-// processRouter.route('/master/:masterRecipeId/recipe/:recipeId/step/:stepId')
-//   .get(authenticate.verifyUser, (req, res, next) => {
-//     User.findOne({_id: req.user.id, masterList: req.params.masterRecipeId})
-//       .then(user => {
-//         return RecipeMaster.findById(req.params.masterRecipeId)
-//           .then(master => {
-//             if (master) {
-//               const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
-//               return Recipe.findById(_recipe)
-//                 .then(recipe => {
-//                   if (recipe) {
-//                     const step = recipe.processSchedule.find(step => step.equals(req.params.stepId));
-//                     if (step) {
-//                       res.statusCode = 200;
-//                       res.setHeader('content-type', 'application/json');
-//                       res.json(step);
-//                     } else {
-//                       const err = new Error(`Step with id: "${req.params.stepId}" not found`);
-//                       err.status = 404;
-//                       return next(err);
-//                     }
-//                   } else {
-//                     const err = new Error(`Recipe with id: "${req.params.recipeId}" not found`);
-//                     err.status = 404;
-//                     return next(err);
-//                   }
-//                 });
-//             } else {
-//               const err = new Error(`Master with id: "${req.params.masterRecipeId}" not found`);
-//               err.status = 404;
-//               return next(err);
-//             }
-//           });
-//       })
-//       .catch(err => next(err));
-//   })
-//   .patch(authenticate.verifyUser, (req, res, next) => {
-//     User.findOne({_id: req.user.id, masterList: req.params.masterRecipeId})
-//       .then(user => {
-//         return RecipeMaster.findById(req.params.masterRecipeId)
-//           .then(master => {
-//             if (master) {
-//               const _recipe = master.recipes.find(item => item.equals(req.params.recipeId));
-//               return Recipe.findById(_recipe)
-//                 .then(recipe => {
-//                   if (recipe) {
-//                     const updateIndex = recipe.processSchedule.findIndex(step => step.equals(req.params.stepId));
-//                     for (const key in req.body) {
-//                       recipe.processSchedule[updateIndex][key] = req.body[key];
-//                     }
-//                     return recipe.save()
-//                       .then(dbres => {
-//                         res.statusCode = 200;
-//                         res.setHeader('content-type', 'application/json');
-//                         res.json(dbres.processSchedule[updateIndex]);
-//                       });
-//                   } else {
-//                     const err = new Error(`Batch with id: "${req.params.recipeId}" not found`);
-//                     err.status = 404;
-//                     return next(err);
-//                   }
-//                 });
-//             } else {
-//               const err = new Error(`Recipe with id: "${req.params.masterRecipeId}" not found`);
-//               err.status = 404;
-//               return next(err);
-//             }
-//           });
-//       })
-//       .catch(err => next(err));
-//   });
 
 module.exports = processRouter;
