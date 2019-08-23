a9fae4ac4bc228a91e0c4ca23bcf07ec245faa04
Andrew Wanex <awanex2705@gmail.com>
2019-08-23 16:24:53 -0700

    refactor: update object mapping function name

--- utils/map-object.js
+++ utils/map-object.js
@@ -1,5 +1,5 @@
 module.exports = (dest, source) => {
-  if (source) {
+  if (dest && source) {
     for (const key in source) {
       if (dest.hasOwnProperty(key)) {
         dest[key] = source[key];
