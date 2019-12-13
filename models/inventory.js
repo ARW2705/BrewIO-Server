55efb4a6862bfb8f2378f47506f7ddfaa6092d07
Andrew Wanex <awanex2705@gmail.com>
2019-12-12 16:00:34 -0800

    refactor: define inventory item model

---
+++ models/inventory.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const inventorySchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  stockCount: {
+    type: Number,
+    default: 0
+  },
+  stockQuantity: {
+    type: Number,
+    default: 0
+  },
+  stockType: {
+    type: String,
+    required: true
+  },
+  labelImageUrl: {
+    type: String
+  },
+  packageDate: {
+    type: Date,
+    required: true
+  },
+  itemDetails: {
+    master: {
+      type: Schema.Types.ObjectId,
+      ref: 'RecipeMaster'
+    },
+    recipe: {
+      type: Schema.Types.ObjectId,
+      ref: 'Recipe'
+    }
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Inventory', inventorySchema);
