28223484977edceb8384ebc8d713ab9ba3d7c979
Andrew Wanex <awanex2705@gmail.com>
2020-05-06 16:06:24 -0700

    refactor: add authorization functions to aid process router

--- routes/process/process-helpers.js
+++ routes/process/process-helpers.js
@@ -1,8 +1,14 @@
-exports.isFriend = (requestingUser, owner) => {
+const isFriend = (requestingUser, owner, recipeMaster) => {
   return  recipeMaster.isFriendsOnly
           && owner.friendsList.some(friend => friend.equals(requestingUser));
+};
+
+const isOwner = (requestingUser, owner) => {
+  return requestingUser === owner;
 }
 
 exports.isUserAuthedForRecipe = (requestingUser, owner, recipeMaster) => {
-  return  recipeMaster.isPublic || isFriend(requestingUser, owner);
+  return  recipeMaster.isPublic
+          || isOwner(requestingUser, owner)
+          || isFriend(requestingUser, owner, recipeMaster);
 };
