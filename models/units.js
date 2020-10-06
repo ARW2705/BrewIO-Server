1d85cb0ffffb38589ea725b2b96bb4714b81be33
Andrew Wanex <awanex2705@gmail.com>
2020-10-06 16:58:18 -0700

    refactor: change units model to store each type of unit

--- models/units.js
+++ models/units.js
@@ -3,23 +3,53 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const unitsSchema = new Schema({
-  volume: {
+const unitSchema = new Schema({
+  system: {
+    type: String,
+    default: 'english standard'
+  },
+  longName: {
     type: String,
     required: true
   },
-  weight: {
+  shortName: {
     type: String,
+    required: true
+  },
+  symbol: {
+    type: String
+  }
+});
+
+const selectedUnitsSchema = new Schema({
+  system: {
+    type: String,
+    default: 'english standard'
+  },
+  weightSmall: {
+    type: unitSchema,
+    required: true
+  },
+  weightLarge: {
+    type: unitSchema,
+    required: true
+  },
+  volumeSmall: {
+    type: unitSchema,
+    required: true
+  },
+  volumeLarge: {
+    type: unitSchema,
     required: true
   },
   temperature: {
-    type: String,
+    type: unitSchema,
     required: true
   },
-  specificGravity: {
-    type: String,
+  density: {
+    type: unitSchema,
     required: true
   }
 });
 
-module.exports = unitsSchema;
+module.exports = selectedUnitsSchema;
