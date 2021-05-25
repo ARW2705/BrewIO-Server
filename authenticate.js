bd6ecfaaf1ba58452399957e31d8e12195d40d9d
Andrew Wanex <awanex2705@gmail.com>
2021-05-25 13:58:14 -0700

    chore: update formatting

--- authenticate.js
+++ authenticate.js
@@ -15,7 +15,7 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = user => {
+exports.getToken = (user) => {
   return jwt.sign(user, TOKEN_KEY, {expiresIn: '30d'});
 };
 
@@ -25,24 +25,30 @@ opts.secretOrKey = TOKEN_KEY;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
   User.findOne({_id: jwt_payload._id}, (error, user) => {
-    if (error) return done(error, false);
-    else if (user) return done(null, user);
-    else return done(null, false);
+    if (error) {
+      return done(error, false);
+    } else if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
   });
 }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
 exports.verifyAdmin = (req, res, next) => {
-  if (req.user.admin) next();
-  else {
+  if (req.user.admin) {
+    next();
+  } else {
     return next(createError(403));
   }
 };
 
 exports.verifyEditor = (req, res, next) => {
-  if (req.user.editor || req.user.admin) next();
-  else {
+  if (req.user.editor || req.user.admin) {
+    next();
+  } else {
     return next(createError(403));
   }
 }
