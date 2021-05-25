8f2a898c8f4c8a5e0b20de2002007966be1d1d44
Andrew Wanex <awanex2705@gmail.com>
2021-05-25 14:17:47 -0700

    feat: add image handling

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const multer = require('multer');
+// const path = require('path');
+// const fs = require('fs');
 
 const User = require('../models/user');
+const httpError = require('../utils/http-error');
 const authenticate = require('../authenticate');
+const imageFileHandler = require('./images/image-helpers');
+const upload = multer(
+  {
+    dest: 'brew-io-uploads/tmp/',
+    fileFilter: (req, file, cb) => {
+      cb(null, file.originalname !== '0.jpg');
+    },
+    limits: {
+      fileSize: 500 * 1024, // 500 kB
+      files: 2
+    }
+  }
+);
+const storeDir = '../../brew-io-uploads/images';
+const imageFields = [
+  { name: 'userImage', maxCount: 1 },
+  { name: 'breweryLabelImage', maxCount: 1 }
+];
 
 const userRouter = express.Router();
 
@@ -62,7 +84,9 @@ userRouter.post('/login', (req, res, next) => {
             friendList: userProfile.friendList,
             token: token,
             preferredUnitSystem: userProfile.preferredUnitSystem,
-            units: userProfile.units
+            units: userProfile.units,
+            userImage: userProfile.userImage,
+            breweryLabelImage: userProfile.breweryLabelImage
           };
           res.statusCode = 200;
           res.setHeader('content-type', 'application/json');
@@ -72,44 +96,80 @@ userRouter.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-userRouter.post('/signup', async(req, res, next) => {
+userRouter.post('/signup', upload.fields(imageFields), async(req, res, next) => {
   const users = await User.find({});
   if (users.length > 20) {
     res.statusCode = 423;
     res.setHeader('content-type', 'application/json');
     return res.json({success: false, status: 'Max users reached for this version: contact admin'});
   }
+
+  const parsedDoc = JSON.parse(req.body.user);
+  req.body['username'] = parsedDoc.username;
+  req.body['password'] = parsedDoc.password;
+
   User.register(
     new User(
       {
-        username: req.body.username,
-        email: req.body.email,
-        preferredUnitSystem: req.body.preferredUnitSystem,
-        units: req.body.units
+        username: parsedDoc.username,
+        email: parsedDoc.email,
+        preferredUnitSystem: parsedDoc.preferredUnitSystem,
+        units: parsedDoc.units
       }
     ),
-    req.body.password,
+    parsedDoc.password,
     (error, user) => {
       if (error) {
         res.statusCode = 500;
         res.setHeader('content-type', 'application/json');
         res.json(error);
       } else {
-        if (req.body.firstname) user.firstname = req.body.firstname;
-        if (req.body.lastname) user.lastname = req.body.lastname;
-        user.save((error, user) => {
-          if (error) {
-            res.statusCode = 500;
-            res.setHeader('content-type', 'application/json');
-            res.json({error: error});
-            return;
-          }
-          passport.authenticate('local')(req, res, () => {
-            res.statusCode = 200;
-            res.setHeader('content-type', 'application/json');
-            res.json({success: true, status: 'Registration successful'});
-          });
-        });
+        if (parsedDoc.firstname) user.firstname = parsedDoc.firstname;
+        if (parsedDoc.lastname) user.lastname = parsedDoc.lastname;
+        if (parsedDoc.userImage) user.userImage = parsedDoc.userImage;
+        if (parsedDoc.breweryLabelImage) user.breweryLabelImage = parsedDoc.breweryLabelImage;
+
+        const storeImages = [];
+
+        if (req.files.userImage && req.files.userImage.length && req.files.userImage[0].cid !== '0') {
+          storeImages.push(imageFileHandler.storeImage(req.files.userImage[0]));
+        }
+
+        if (req.files.breweryLabelImage && req.files.breweryLabelImage.length && req.files.breweryLabelImage[0].cid !== '0') {
+          storeImages.push(imageFileHandler.storeImage(req.files.breweryLabelImage[0]));
+        }
+
+        Promise.all(storeImages)
+          .then(storedImages => {
+            storedImages.forEach(storedImage => {
+              let fieldname = 'userImage';
+              if (storedImage.fieldname === fieldname) {
+                user[fieldname].serverFilename = storedImage.filename;
+                user[fieldname].hasPending = false;
+              }
+
+              fieldname = 'breweryLabelImage';
+              if (storedImage.fieldname === fieldname) {
+                user[fieldname].serverFilename = storedImage.filename;
+                user[fieldname].hasPending = false;
+              }
+            });
+
+            user.save((error, user) => {
+              if (error) {
+                res.statusCode = 500;
+                res.setHeader('content-type', 'application/json');
+                res.json({error: error});
+                return;
+              }
+              passport.authenticate('local')(req, res, () => {
+                res.statusCode = 200;
+                res.setHeader('content-type', 'application/json');
+                res.json({success: true, status: 'Registration successful'});
+              });
+            });
+          })
+          .catch(next);
       }
     });
 });
@@ -117,12 +177,6 @@ userRouter.post('/signup', async(req, res, next) => {
 userRouter.route('/profile')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findById(req.user.id)
-      .populate('masterList.style')
-      .populate('masterList.variants.grains.grainType')
-      .populate('masterList.variants.hops.hopsType')
-      .populate('masterList.variants.yeast.yeastType')
-      .populate('activeBatchList.recipe')
-      .populate('inventoryList.itemDetails.master inventoryList.itemDetails.recipe')
       .then(user => {
         if (user !== null) {
 
@@ -133,31 +187,70 @@ userRouter.route('/profile')
             lastname: user.lastname,
             email: user.email,
             preferredUnitSystem: user.preferredUnitSystem,
-            units: user.units
+            units: user.units,
+            userImage: user.userImage,
+            breweryLabelImage: user.breweryLabelImage
           };
           res.statusCode = 200;
           res.setHeader('content-type', 'application/json');
           res.json(response);
         } else {
-          return next(createError(404, 'User not found'));
+          throw httpError(404, 'User not found');
         }
       })
       .catch(error => next(error));
   })
-  .patch(authenticate.verifyUser, (req, res, next) => {
-    User.findByIdAndUpdate(req.user.id, req.body, {new: true})
-      .then(update => {
-        if (update !== null) {
-          res.statusCode = 200;
-          res.setHeader('content-type', 'application/json');
-          res.json(update);
-        } else {
-          return next(createError(500, 'Failed to update User'));
-        }
-      })
-      .catch(error => next(error));
-  });
+  .patch(authenticate.verifyUser, upload.fields(imageFields), (req, res, next) => {
+    const parsedDoc = JSON.parse(req.body.user);
 
-// TODO add user profile routes
+    User.findByIdAndUpdate(req.user.id, { $set: parsedDoc }, { new: true })
+      .then(updated => {
+        const storeImages = [];
+
+        if (req.files.userImage && req.files.userImage.length && req.files.userImage[0].cid !== '0') {
+          storeImages.push(imageFileHandler.storeImage(req.files.userImage[0]));
+        }
+
+        if (req.files.breweryLabelImage && req.files.breweryLabelImage.length && req.files.breweryLabelImage[0].cid !== '0') {
+          storeImages.push(imageFileHandler.storeImage(req.files.breweryLabelImage[0]));
+        }
+
+        return Promise.all(storeImages)
+          .then(storedImages => {
+            const deleteImages = [];
+
+            storedImages.forEach(storedImage => {
+              let fieldname = 'userImage';
+
+              if (storedImage.fieldname === fieldname) {
+                deleteImages.push(
+                  imageFileHandler.deleteImage(updated[fieldname].serverFilename)
+                );
+
+                updated[fieldname].serverFilename = storedImage.filename;
+                updated[fieldname].hasPending = false;
+              }
+
+              fieldname = 'breweryLabelImage';
+              if (storedImage.fieldname === fieldname) {
+                deleteImages.push(
+                  imageFileHandler.deleteImage(updated[fieldname].serverFilename)
+                );
+                updated[fieldname].serverFilename = storedImage.filename;
+                updated[fieldname].hasPending = false;
+              }
+            });
+
+            return Promise.all([ updated.save(), ...deleteImages ]);
+          });
+      })
+      .then(results => {
+        const completedUser = results[0];
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        res.json(completedUser);
+      })
+      .catch(next);
+  });
 
 module.exports = userRouter;
