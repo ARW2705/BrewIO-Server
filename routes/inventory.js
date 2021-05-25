88e7fdbef9609c11f1c1717102c52ea7ae02b9d8
Andrew Wanex <awanex2705@gmail.com>
2021-05-25 14:09:59 -0700

    feat: add image handling to inventory route

--- routes/inventory.js
+++ routes/inventory.js
@@ -4,11 +4,32 @@ const httpError = require('../utils/http-error');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
 const authenticate = require('../authenticate');
 const Inventory = require('../models/inventory');
 const User = require('../models/user');
 const Recipe = require('../models/recipe-master');
+const imageFileHandler = require('./images/image-helpers');
+const upload = multer(
+  {
+    dest: 'brew-io-uploads/tmp/',
+    fileFilter: (req, file, cb) => {
+      cb(null, file.originalname !== '0.jpg');
+    },
+    limits: {
+      fileSize: 500 * 1024, // 500 kB
+      files: 2
+    }
+  }
+);
+const storeDir = '../../brew-io-uploads/images';
+const inventoryFields = [
+  { name: 'itemLabelImage', maxCount: 1 },
+  { name: 'supplierLabelImage', maxCount: 1 }
+];
 
 const inventoryRouter = express.Router();
 
@@ -33,22 +54,55 @@ inventoryRouter.route('/')
       })
       .catch(next);
   })
-  .post(authenticate.verifyUser, (req, res, next) => {
+  .post(authenticate.verifyUser, upload.fields(inventoryFields), (req, res, next) => {
     User.findById(req.user.id)
       .then(user => {
         if (user === null) {
           throw httpError(404, 'Could not find user record');
         }
-        return Inventory.create(req.body)
+
+        const parsedDoc = JSON.parse(req.body.inventoryItem);
+
+        return Inventory.create(parsedDoc)
           .then(newItem => {
-            user.inventoryList.push(newItem);
+            const storeImages = [];
+
+            if (req.files.itemLabelImage && req.files.itemLabelImage.length && req.files.itemLabelImage[0].cid !== '0') {
+              storeImages.push(imageFileHandler.storeImage(req.files.itemLabelImage[0]));
+            }
+
+            if (req.files.supplierLabelImage && req.files.supplierLabelImage.length && req.files.supplierLabelImage[0].cid !== '0') {
+              storeImages.push(imageFileHandler.storeImage(req.files.supplierLabelImage[0]));
+            }
+
+            return Promise.all(storeImages)
+              .then(storedImages => {
+                storedImages.forEach(storedImage => {
+                  if (storedImage.fieldname === 'itemLabelImage') {
+                    newItem.optionalItemData.itemLabelImage.serverFilename
+                      = storedImage.filename;
+                    newItem.optionalItemData.itemLabelImage.hasPending = false;
+                  }
+
+                  if (storedImage.fieldname === 'supplierLabelImage') {
+                    newItem.optionalItemData.supplierLabelImage.serverFilename
+                      = storedImage.filename;
+                    newItem.optionalItemData.supplierLabelImage.hasPending = false;
+                  }
+                });
+
+                return newItem.save();
+              });
+          })
+          .then(completedItem => {
+            user.inventoryList.push(completedItem);
             return user.save()
               .then(_update => {
                 res.statusCode = 201;
                 res.setHeader('content-type', 'application/json');
-                res.json(newItem);
-              })
-          })
+                res.json(completedItem);
+              });
+          });
       })
       .catch(next);
   });
@@ -67,26 +121,75 @@ inventoryRouter.route('/:itemId')
               res.setHeader('content-type', 'application/json');
               return res.json(item);
             }
-            throw httpError(404, 'Could not find batch record');
+            throw httpError(404, 'Could not find inventory record');
           });
       })
       .catch(error => next(error));
   })
-  .patch(authenticate.verifyUser, (req, res, next) => {
+  .patch(authenticate.verifyUser, upload.fields(inventoryFields), (req, res, next) => {
     User.findById(req.user.id)
       .then(user => {
         if (user === null) {
           throw httpError(404, 'Could not find user record');
         }
-        return Inventory.findByIdAndUpdate(req.params.itemId, { $set: req.body }, {new: true});
-      })
-      .then(updatedItem => {
-        if (updatedItem === null) {
-          throw httpError(404, 'Could not find batch record');
-        }
-        res.statusCode = 200;
-        res.setHeader('content-type', 'application/json');
-        res.json(updatedItem);
+
+        const parsedDoc = JSON.parse(req.body.inventoryItem);
+
+        return Inventory.findByIdAndUpdate(req.params.itemId, { $set: parsedDoc }, {new: true})
+          .then(updatedItem => {
+            const storeImages = [];
+
+            if (req.files.itemLabelImage && req.files.itemLabelImage.length && req.files.itemLabelImage[0].cid !== '0') {
+              storeImages.push(imageFileHandler.storeImage(req.files.itemLabelImage[0]));
+            }
+
+            if (req.files.supplierLabelImage && req.files.supplierLabelImage.length && req.files.supplierLabelImage[0].cid !== '0') {
+              storeImages.push(imageFileHandler.storeImage(req.files.supplierLabelImage[0]));
+            }
+
+            return Promise.all(storeImages)
+              .then(storedImages => {
+                const deleteImages = [];
+
+                storedImages.forEach(storedImage => {
+                  if (storedImage.fieldname === 'itemLabelImage') {
+                    deleteImages.push(
+                      imageFileHandler.deleteImage(
+                        updatedItem.optionalItemData.itemLabelImage.serverFilename
+                      )
+                    );
+
+                    updatedItem.optionalItemData.itemLabelImage.serverFilename
+                      = storedImage.filename;
+                    updatedItem.optionalItemData.itemLabelImage.hasPending = false;
+                  }
+
+                  if (storedImage.fieldname === 'supplierLabelImage') {
+                    deleteImages.push(
+                      imageFileHandler.deleteImage(
+                        updatedItem.optionalItemData.supplierLabelImage.serverFilename
+                      )
+                    );
+
+                    updatedItem.optionalItemData.supplierLabelImage.serverFilename
+                      = storedImage.filename;
+                    updatedItem.optionalItemData.supplierLabelImage.hasPending = false;
+                  }
+                });
+
+                return Promise.all([ updatedItem.save(), ...deleteImages ]);
+              });
+          })
+          .then(results => {
+            const completedItem = results[0];
+            user.inventoryList.push(completedItem);
+            return user.save()
+              .then(_update => {
+                res.statusCode = 200;
+                res.setHeader('content-type', 'application/json');
+                res.json(completedItem);
+              });
+          });
       })
       .catch(next);
   })
@@ -96,20 +199,38 @@ inventoryRouter.route('/:itemId')
         if (user === null) {
           throw httpError(404, 'Could not find user record');
         }
+
         return Inventory.findByIdAndDelete(req.params.itemId)
           .then(dbres => {
             if (dbres === null) {
-              throw httpError(404, 'Could not find batch record');
+              throw httpError(404, 'Could not find inventory record');
             }
-            const toDeleteIndex = user.inventoryList.findIndex(item => {
-              return item._id.equals(req.params.itemId);
-            });
-            user.inventoryList.splice(toDeleteIndex, 1);
-            return user.save()
+
+            const deleteImages = [];
+
+            if (dbres.optionalItemData && dbres.optionalItemData.itemLabelImage) {
+              deleteImages.push(imageFileHandler.deleteImage(dbres.optionalItemData.itemLabelImage.serverFilename));
+            }
+
+            if (dbres.optionalItemData && dbres.optionalItemData.supplierLabelImage) {
+              deleteImages.push(imageFileHandler.deleteImage(dbres.optionalItemData.supplierLabelImage.serverFilename));
+            }
+
+            return Promise.all(deleteImages)
               .then(() => {
-                res.statusCode = 200;
-                res.setHeader('content-type', 'application/json');
-                res.json(dbres);
+                const toDeleteIndex = user.inventoryList
+                  .findIndex(item => {
+                    return item._id.equals(req.params.itemId);
+                  });
+
+                user.inventoryList.splice(toDeleteIndex, 1);
+
+                return user.save()
+                  .then(() => {
+                    res.statusCode = 200;
+                    res.setHeader('content-type', 'application/json');
+                    res.json(dbres);
+                  });
               });
           });
       })
